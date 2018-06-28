3fc491478aa2d5b8fb8f66db2e829d3b184dd1a5
yohanes <ryushi00617@yahoo.co.jp>
2018-06-28 17:02:23 +0900

    Ui周りの追加

---
+++ Woods/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+
+    public GameObject player;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x, 0, -10);
+        if (player.transform.position.y > 0)
+        {
+            transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+        }
+    }
+}
---
+++ Woods/Assets/Scripts/Getnut.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Getnut : MonoBehaviour {
+    public Text ScoreText;
+    private int Score;
+    
+    // Use this for initialization
+	void Start () {
+        Score = 1;
+        SetScore();
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+           gameObject.SetActive(false);
+            Score -= 1;
+        }
+            SetScore();
+    }
+
+    void SetScore()
+    {
+       ScoreText.text = "木の実残り"+Score.ToString();
+        if (Score == 0)
+        {
+
+        }
+    }
+}
